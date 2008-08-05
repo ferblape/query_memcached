870b4ee91845ccd0990905835a27ee4aa7c0c46f
Fernando Blat Peris <ferblape@gmail.com>
2008-08-05 18:47:39 +0200

    Cleaned files and dirs

---
+++ lib/query_memcached.rb
@@ -0,0 +1,215 @@
+require 'digest/md5'
+
+unless defined?(Cache)
+  warning = "[Query cache WARNING] Cache object is not defined"
+  puts warning
+  ActiveRecord::Base.logger.error warning
+end 
+
+module ActiveRecord
+    
+  class Base
+    
+    class << self
+      def cache_version_key      
+        "version/#{self.table_name}" 
+      end
+    end
+    
+    def cache_version_key
+      return nil unless self.id      
+      "version/#{self.class.table_name}/#{self.id}"
+    end      
+            
+    def create_or_update_with_clean_query_cache(*args)
+      increase_version! if defined?(Cache)
+      create_or_update_without_clean_query_cache(*args)
+    end
+
+    alias_method_chain :create_or_update, :clean_query_cache
+    
+    def increase_version!
+      # Increment the class version key number
+      key = self.class.cache_version_key
+      if r = Cache.get(key).to_i
+        Cache.set(key,(r+1%10000000))
+      else
+        Cache.set(key,1)
+      end
+    end
+            
+  end  
+end
+
+
+module ActiveRecord
+  module ConnectionAdapters # :nodoc:
+    module QueryCache
+      
+      class << self
+        def included(base)
+          base.class_eval do
+            attr_accessor :query_cache_enabled
+            alias_method_chain :columns, :query_cache
+            alias_method_chain :select_all, :query_cache                        
+          end
+
+          dirties_query_cache base, :insert, :update, :delete
+        end
+
+        def dirties_query_cache(base, *method_names)
+          method_names.each do |method_name|
+            base.class_eval <<-end_code, __FILE__, __LINE__
+              def #{method_name}_with_query_dirty(*args)
+                clear_query_cache if @query_cache_enabled
+                #{method_name}_without_query_dirty(*args)
+              end
+
+              alias_method_chain :#{method_name}, :query_dirty
+            end_code
+          end
+        end
+      end
+
+      # Enable the query cache within the block
+      def cache
+        old, @query_cache_enabled = @query_cache_enabled, true
+        @query_cache ||= {}
+        @cache_version ||= {}
+        yield
+      ensure
+        clear_query_cache
+        @query_cache_enabled = old
+      end
+
+      # Disable the query cache within the block.
+      def uncached
+        old, @query_cache_enabled = @query_cache_enabled, false
+        yield
+      ensure
+        @query_cache_enabled = old
+      end
+
+      # Clears the query cache.
+      #
+      # One reason you may wish to call this method explicitly is between queries
+      # that ask the database to randomize results. Otherwise the cache would see
+      # the same SQL query and repeatedly return the same result each time, silently
+      # undermining the randomness you were expecting.
+      def clear_query_cache
+        @query_cache.clear if @query_cache
+        @cache_version.clear if @cache_version
+      end
+
+      def select_all_with_query_cache(*args)
+        if @query_cache_enabled          
+          cache_sql(args.first) { select_all_without_query_cache(*args) }
+        else
+          select_all_without_query_cache(*args)
+        end
+      end
+
+      def columns_with_query_cache(*args)
+        if @query_cache_enabled
+          @query_cache["SHOW FIELDS FROM #{args.first}"] ||= columns_without_query_cache(*args)
+        else
+          columns_without_query_cache(*args)
+        end
+      end
+      
+      private
+      
+        def cache_sql(sql)
+          # priority order:
+          #  - if in @query_cache (memory of local app server) we prefer this
+          #  - else if exists in Memcached we prefer that 
+          #  - else perform query in database and save memory caches
+          result =
+            if @query_cache.has_key?(sql)
+              log_info(sql, "CACHE", 0.0)
+              @query_cache[sql]
+            elsif defined?(Cache) and cached_result = Cache.get(query_key(sql))
+              log_info(sql, "MEMCACHE", 0.0)
+              @query_cache[sql] = cached_result
+              cached_result
+            else
+              query_result = yield
+              @query_cache[sql] = query_result
+              Cache.set(query_key(sql),query_result) if defined?(Cache)
+              query_result
+            end
+
+          if Array === result
+            result.collect { |row| row.dup }
+          else
+            result.duplicable? ? result.dup : result
+          end
+        rescue TypeError
+          result
+        end
+        
+        # Transforms a sql query into a valid key for Memcache                
+        def query_key(sql)
+          table_names, clean_query = extract_table_names(sql)
+          # version_number is the sum of the global version number and all the version number of each table
+          version_number = get_cache_version
+          table_names.each { |table_name| version_number += get_cache_version(table_name) }
+          
+          # check if the result_key is short enough for memcache key length limit (250 char)
+          result_key = "#{version_number}_#{clean_query}"          
+          if result_key.length + Cache.namespace.length + 1  >= 250 
+            result_key = "#{version_number}_#{Digest::MD5.hexdigest(clean_query)}"            
+          end
+          result_key
+        # rescue
+        #   raise TypeError
+        end
+                
+        # Returns the cache version of a table_name. If table_name is empty its the global version
+        # 
+        # We prefer to search for this key first in memory and then in Memcache
+        def get_cache_version(table_name = nil)
+          key_class_version = "version"
+          key_class_version << "/#{table_name}" if table_name
+          if @cache_version && @cache_version[key_class_version]
+            @cache_version[key_class_version]
+          elsif version = Cache.get(key_class_version)
+            @cache_version[key_class_version] = version if @cache_version
+            version
+          else 
+            @cache_version[key_class_version] = 0 if @cache_version
+            Cache.set(key_class_version, 0) or 0
+          end
+        end
+        
+        def extract_table_names(sql)
+          table_names = []
+          # strip all whitespaces in one whitespace
+          # substitute whitespace for '_'
+          # remove '`'           
+          sql.gsub!(/`/,'')
+          clean_query = sql.gsub(/[ ]+/,'_').gsub(/`/,'')
+          selectors = /((WHERE|HAVING|LIMIT|ORDER BY|GROUP BY|ON).*)+/i
+          matched = sql.scan(/from ([\w\d\_\-\, ]+) ?#{selectors}?$/i).first.first.gsub(/#{selectors}/,'').gsub(/(\w+) ?JOIN/i,'').strip
+          # if the from statment is multi-table
+          if matched.split(',').size > 1
+            matched.split(',').each do |table_name|
+              parts = table_name.strip.split(' ')
+              if parts.size > 1
+                table_names << parts.first.strip
+              else
+                table_names << table_name.strip
+              end
+            end
+          elsif 
+            matched.split(' ').each { |table_name| table_names << table_name.strip }
+          else
+            table_names << matched
+          end
+          return table_names, clean_query
+        end
+
+    end
+  end
+end
+
---
+++ test/query_memcached_test.rb
@@ -0,0 +1,38 @@
+require 'test/unit'
+require File.expand_path(File.join(File.dirname(__FILE__), '../../../../config/environment.rb'))
+
+class Item < ActiveRecord::Base
+end
+
+class QueryMemcachedTest < Test::Unit::TestCase
+  
+  def test_extract_table_names
+    private_methods = ActiveRecord::ConnectionAdapters::QueryCache.private_instance_methods    
+    ActiveRecord::ConnectionAdapters::QueryCache.class_eval { public *private_methods }
+    ActiveRecord::ConnectionAdapters::QueryCache.class_eval { module_function :extract_table_names }    
+    
+    
+    p  = "select * from countries"
+    q  = "SELECT_*_FROM_countries_"
+    p2 = "select * from ratings where (ratings.item_id = 36) AND (ratings.user_id = 11) LIMIT 1"
+    q2 = "SELECT_*_FROM_ratings_WHERE_(ratings.item_id_=_36)_AND_(ratings.user_id_=_11)_LIMIT_1" 
+    p3 = "SELECT * from pictures where (pictures.place_id = 3)"
+    q3 = "SELECT_*_FROM_pictures_WHERE_(pictures.place_id_=_3)_"
+    p4 = "select distinct(p.id) from places p, users u, places_users pu, where pu.user_id = u.id and pu.place_id = p.id and p.id != 3"
+    q4 = "0_select_distinct(p.id)_from_places_p,_users_u,_places_users_pu_where_pu.user_id_=_u.id_and_pu.place_id_=_p.id_and_p.id_!=_3"
+    p5 = "select count(*) as count_all from places inner join places_users on places_id = places_users.place_id where (place_id = 3) AND (places_users.user_id = 11)"
+    q5 = "0_SELECT_count(*)_AS_count_all_FROM_places_INNER_JOIN_places_users_ON_places.id_=_places_users.place_id_WHERE_(place_id_=_3)_AND_(places_users.user_id_=_11_)_"
+    p6 = "select * from items order by created_at DESC limit 10"
+    q6 = "SELECT_*_FROM_items_ORDER_BY_created_at_DESC_LIMIT_10"
+    p7 = "SELECT * from countries where id IN (SELECT * from places where place_id = country.id)"
+    
+    assert_equal ActiveRecord::ConnectionAdapters::QueryCache.extract_table_names(p).first,  ['countries']
+    assert_equal ActiveRecord::ConnectionAdapters::QueryCache.extract_table_names(p2).first, ['ratings']
+    assert_equal ActiveRecord::ConnectionAdapters::QueryCache.extract_table_names(p3).first, ['pictures']
+    assert_equal ActiveRecord::ConnectionAdapters::QueryCache.extract_table_names(p4).first, ['places', 'users', 'places_users']
+    assert_equal ActiveRecord::ConnectionAdapters::QueryCache.extract_table_names(p5).first, ['places', 'places_users']
+    assert_equal ActiveRecord::ConnectionAdapters::QueryCache.extract_table_names(p6).first, ['items']
+    assert_equal ActiveRecord::ConnectionAdapters::QueryCache.extract_table_names(p7).first, ['countries', 'places']
+  end
+  
+end
